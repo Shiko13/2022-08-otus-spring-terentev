b963faf81ea55705a4605a228334f83947c0b05b
Sergey <shikofreid@yandex.ru>
2022-11-22 23:45:41 +0400

    Initial commit

---
+++ Homework_5/src/main/java/ru/otus/spring/homework_5/Homework5Application.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.homework_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework5Application.class, args);
+	}
+
+}
---
+++ Homework_5/src/test/java/ru/otus/spring/homework_5/Homework5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.homework_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
