c7675e58c94eb51986f31ff1956fff4940148305
Sergey <shikofreid@yandex.ru>
2022-12-14 13:05:34 +0400

    Modified with @EntityGraph

--- Homework_7/src/main/java/ru/otus/spring/repository/BookRepository.java
+++ Homework_7/src/main/java/ru/otus/spring/repository/BookRepository.java
@@ -1,8 +1,7 @@
 package ru.otus.spring.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.otus.spring.domain.Book;
 
 import java.util.List;
@@ -10,9 +9,9 @@ import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    @Query("select b from Book b join fetch b.author join fetch b.genre where b.id = :id")
-    Optional<Book> findById(@Param("id") long id);
+    @EntityGraph(attributePaths = {"author", "genre"})
+    Optional<Book> findById(long id);
 
-    @Query("select distinct b from Book b join fetch b.author join fetch b.genre")
+    @EntityGraph(attributePaths = {"author", "genre"})
     List<Book> findAll();
 }
