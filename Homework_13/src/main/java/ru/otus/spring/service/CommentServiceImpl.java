c3ee300e7af265d5ac13efdd855ff544ad7d0ad8
Sergey <shikofreid@yandex.ru>
2023-01-15 21:39:56 +0400

    Added AuthenticationService

--- Homework_13/src/main/java/ru/otus/spring/service/CommentServiceImpl.java
+++ Homework_13/src/main/java/ru/otus/spring/service/CommentServiceImpl.java
@@ -3,16 +3,6 @@ package ru.otus.spring.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.acls.domain.BasePermission;
-import org.springframework.security.acls.domain.CumulativePermission;
-import org.springframework.security.acls.domain.ObjectIdentityImpl;
-import org.springframework.security.acls.domain.PrincipalSid;
-import org.springframework.security.acls.model.MutableAcl;
-import org.springframework.security.acls.model.MutableAclService;
-import org.springframework.security.acls.model.ObjectIdentity;
-import org.springframework.security.acls.model.Sid;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.otus.spring.domain.Book;
@@ -34,7 +24,7 @@ public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
     private final BookRepository bookRepository;
     private final DtoConverter<Comment, CommentDto> commentConverter;
-    private final MutableAclService mutableAclService;
+    private final AuthenticationService authenticationService;
 
 
     @Override
@@ -44,33 +34,7 @@ public class CommentServiceImpl implements CommentService {
         populateBook(comment);
         CommentDto insertedComment = commentConverter.toDto(commentRepository.save(comment));
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Sid owner = new PrincipalSid(authentication);
-        ObjectIdentity oid = new ObjectIdentityImpl(insertedComment.getClass(), insertedComment.getId());
-
-        String adminPrincipal = "admin";
-        Sid admin = new PrincipalSid(adminPrincipal);
-        MutableAcl acl = mutableAclService.createAcl(oid);
-        acl.setOwner(owner);
-        CumulativePermission cumulativePermissionForAdmin = new CumulativePermission();
-        cumulativePermissionForAdmin.set(BasePermission.ADMINISTRATION);
-        cumulativePermissionForAdmin.set(BasePermission.CREATE);
-        cumulativePermissionForAdmin.set(BasePermission.DELETE);
-        cumulativePermissionForAdmin.set(BasePermission.READ);
-        cumulativePermissionForAdmin.set(BasePermission.WRITE);
-        acl.insertAce(acl.getEntries().size(), cumulativePermissionForAdmin, admin, true);
-
-        if (!adminPrincipal.equals(authentication.getName())) {
-            CumulativePermission cumulativePermissionForOwner = new CumulativePermission();
-            cumulativePermissionForOwner.set(BasePermission.CREATE);
-            cumulativePermissionForOwner.set(BasePermission.DELETE);
-            cumulativePermissionForOwner.set(BasePermission.READ);
-            cumulativePermissionForOwner.set(BasePermission.WRITE);
-            acl.insertAce(acl.getEntries().size(), cumulativePermissionForOwner, owner, true);
-        }
-        mutableAclService.updateAcl(acl);
-
-        return insertedComment;
+        return authenticationService.setPermission(insertedComment);
     }
 
 
